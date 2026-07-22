c300c6cf35d608c4fdde568c10663dc67f408c68
Sulabh312 <135011851+Sulabh312@users.noreply.github.com>
2026-07-22 15:21:44 +0530

    Time: 0 ms (100%), Space: 21.8 MB (48.78%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        vector<vector<int>> ans;
-        sort(intervals.begin(), intervals.end());
-        int minVal = intervals[0][0];
-        int maxVal = intervals[0][1];
+        // intervals.push_back(newInterval);
+        // vector<vector<int>> ans;
+        // sort(intervals.begin(), intervals.end());
+        // int minVal = intervals[0][0];
+        // int maxVal = intervals[0][1];
+        // int n = intervals.size();
+        // for(int i = 0; i < n; i++){
+        //     if(intervals[i][0] > maxVal){
+        //         ans.push_back({minVal, maxVal});
+        //         minVal = intervals[i][0];
+        //         maxVal = intervals[i][1];
+        //     }
+        //     else maxVal = max(maxVal, intervals[i][1]);
+        // }
+        // ans.push_back({minVal, maxVal});
+        // return ans;
         int n = intervals.size();
-        for(int i = 0; i < n; i++){
-            if(intervals[i][0] > maxVal){
-                ans.push_back({minVal, maxVal});
-                minVal = intervals[i][0];
-                maxVal = intervals[i][1];
-            }
-            else maxVal = max(maxVal, intervals[i][1]);
+        int i = 0;
+        vector<vector<int>> ans;
+        while(i < n && newInterval[0] > intervals[i][1]){
+            ans.push_back(intervals[i]);
+            i++;
         }
-        ans.push_back({minVal, maxVal});
+        while(i < n && newInterval[1]>= intervals[i][0]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+        while(i < n){
+            ans.push_back(intervals[i]);
+            i++;
+        }
         return ans;
     }
 };
