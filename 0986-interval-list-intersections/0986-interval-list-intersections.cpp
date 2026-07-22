aab11644a1eb512ba40f6d77707c5ff17ec9cfb5
Sulabh312 <135011851+Sulabh312@users.noreply.github.com>
2026-07-22 16:41:02 +0530

    Time: 0 ms (100%), Space: 22.9 MB (60.5%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& fL, vector<vector<int>>& sL) {
         vector<vector<int>> ans;
         int fN = fL.size(), sN = sL.size();
-        if(fN == 0 || sN == 0) return {};
         for(int i = 0, j = 0; i < fN && j< sN; ){
             int start = max(fL[i][0], sL[j][0]);
             int end = min(fL[i][1], sL[j][1]);
