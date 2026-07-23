1d17dec03c4b3e37761f8726d9c1a1ee092faa54
Sulabh312 <135011851+Sulabh312@users.noreply.github.com>
2026-07-23 07:11:51 +0530

    Time: 0 ms (100%), Space: 198.8 MB (60.63%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -4,11 +4,7 @@ public:
         int ans = 1;
         int n = nums.size();
         if(n <= 2) return n;
-        while(ans <= n) {
-            ans <<= 1; 
-            cout << ans << endl;
-            cout << (ans < n? 1: 0 )<< endl;
-        }
+        while(ans <= n)  ans <<= 1; 
         return ans;
     }
 };
