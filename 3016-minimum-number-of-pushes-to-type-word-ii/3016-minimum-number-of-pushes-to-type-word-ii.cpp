87cdc5031494f586c5b5ad185fff3a448619bcb4
Sulabh312 <135011851+Sulabh312@users.noreply.github.com>
2026-07-31 09:12:19 +0530

    Time: 96 ms (5.28%), Space: 27.3 MB (36.52%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -4,16 +4,13 @@ public:
         int n = word.length();
         map<char, int> mp;
         for(char c : word) mp[c]++;
-        // if(mp.size() < 9) return n;
+        if(mp.size() < 9) return n;
         vector<pair<char, int>> vec(mp.begin(), mp.end());
         sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) {
             return a.second > b.second;
         });
         int ans = 0;
-        for(int i = 0; i < vec.size(); i++){
-            ans += vec[i].second *  (i/8+1) ;
-            // cout << vec[i].first << ':' << vec[i].second << ':' << i/8  << ':' << ans<< endl;
-        }
+        for(int i = 0; i < vec.size(); i++) ans += vec[i].second *  (i/8+1) ;
         return ans;
     }
 };
