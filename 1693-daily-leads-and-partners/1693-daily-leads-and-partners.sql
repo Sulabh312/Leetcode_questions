9a96175d90c7d52717ce3335d6333072ecb788f1
Sulabh Gupta <135011851+Sulabh312@users.noreply.github.com>
2026-09-02 23:25:32 +0530

    Time: 617 ms (45.73%), Space: 0B (100%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -3,5 +3,5 @@ SELECT date_id,
        make_name, 
        count(DISTINCT lead_id) AS unique_leads,
        count(DISTINCT partner_id ) AS unique_partners
-       FROM DailySales
-       GROUP BY date_id, make_name;
+FROM DailySales
+GROUP BY date_id, make_name;
